92ac3b9bb7df08c2b7bc304a935bd133c1193d71
Matthew Cross <matthewcross1017@gmail.com>
2022-02-28 23:16:51 -0500

    change api

--- client/src/context/wine/WineContext.js
+++ client/src/context/wine/WineContext.js
@@ -2,7 +2,7 @@ import { createContext, useReducer } from 'react'
 import wineReducer from './WineReducer'
 const WineContext = createContext()
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const WineProvider = ({ children }) => {
     const initialState = {
         users: [],
@@ -13,20 +13,22 @@ export const WineProvider = ({ children }) => {
     const searchUsers = async (text) => {
         setLoading()
         const params = new URLSearchParams({
-            q: text
+            q: text,
         })
+        const modifyParams = params.toString()
+        const searchParams = modifyParams.substring(2)
         const response = await
-        fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
+        fetch(`${GITHUB_URL}/${searchParams}`)
         const {items} = await response.json()
+        console.log(response)
        dispatch({
           type: 'GET_USERS',
           payload: items,
       })
+    
     }
+
+
  // Clear  user from state
  const clearUsers = () => dispatch({type: 'CLEAR_USERS' })
     // set loading
